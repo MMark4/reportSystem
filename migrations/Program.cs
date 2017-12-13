096c5b13a1f0cff42e920d777b3533d5ae7ef203
Saetabis1 <marca.manu2@gmail.com>
2017-12-12 22:07:11 -0300

    added scripts

---
+++ migrations/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace migrations
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using DbUp;
+    using Microsoft.Extensions.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config =  new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var conectionStrings = config.GetSection("ConnectionStrings");
+
+            var upgrader = DeployChanges.To
+                    .SqlDatabase(conectionStrings["DefaultConnection:"])
+                    .WithScriptsFromFileSystem(AppDomain.CurrentDomain.BaseDirectory + "scripts/*.sql")
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();                
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+    }
+}
